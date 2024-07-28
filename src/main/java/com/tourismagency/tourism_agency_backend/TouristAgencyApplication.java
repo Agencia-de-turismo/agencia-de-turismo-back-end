1345c3164795d9ae75234b851410340d5772fd90
IsraelTeo <israel.bastion123@gmail.com>
2024-07-28 04:06:06 -0500

    Basic scaffolding added.

--- src/main/java/com/tourismagency/tourism_agency_backend/TouristAgencyApplication.java
+++ src/main/java/com/tourismagency/tourism_agency_backend/TouristAgencyApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TouristAgencyApplication {
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             SpringApplication.run(TouristAgencyApplication.class, args);
         }
-
 
 }
---
+++ src/main/java/com/tourismagency/tourism_agency_backend/persistence/model/TouristService.java
@@ -0,0 +1,4 @@
+package com.tourismagency.tourism_agency_backend.persistence.model;
+
+public class TouristService {
+}
